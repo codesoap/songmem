09315c584a83cdbe01b604efe9e7c13239fb6a11
codesoap <codesoap@mailbox.org>
2019-11-02 11:52:27 +0100

    Add cmd

--- README.md
+++ README.md
@@ -1,27 +1,24 @@
 [![GoDoc](https://godoc.org/github.com/codesoap/songs?status.svg)](https://godoc.org/github.com/codesoap/songs)
 
 # Usage
-```console
-$ # Add new songs:
-$ songs --insert 'Le Wanski - Trauma (N'to - Trauma Remix)'
-$ songs --insert 'MALO - March of Progress'
-$ # List songs (newest additions to oldest):
-$ songs
-MALO - March of Progress
-Le Wanski - Trauma (N'to - Trauma Remix)
-$ # Register, that you are listening to a song (you might use this in
-$ # scripts):
-$ songs --register 'Le Wanski - Trauma (N'to - Trauma Remix)'
-$ # List those songs first, that you listened to the most:
-$ songs --favourites
-Le Wanski - Trauma (N'to - Trauma Remix)
-MALO - March of Progress
-$ # List songs by frecency (songs you lately listened to a lot):
-$ songs --frecent
-Le Wanski - Trauma (N'to - Trauma Remix)
-MALO - March of Progress
-$ # List songs, that you often listen to before or after listening to
-$ # the given song:
-$ songs --similar 'Le Wanski - Trauma (N'to - Trauma Remix)'
-MALO - March of Progress
+```
+Usage:
+    songs --register [--no-add] <name>
+    songs
+    songs --added-at
+    songs --favourite
+    songs --frecent
+    songs --suggestions <name>
+Options:
+    -h --help         Show this screen.
+    -r --register     Register that you just heard a song. If the song does not
+                      exist yet, it will be added.
+    -n --no-add       Do not add a song to the database, when registering that
+                      you just heard it. If the song does not exist, nothing
+                      will happen.
+    -t --added-at     List songs by the date of their addition. Newest first.
+    -f --favourite    List songs you heard the most. Most heard first.
+    -c --frecent      List songs you recently heard a lot. Most frecent first.
+    -s --suggestions  List songs, that you often hear before or after hearing
+                      the given song. Best suggestions first.
 ```
---
+++ cmd/main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"fmt"
+	"github.com/codesoap/songs"
+	"github.com/docopt/docopt-go"
+	"os"
+)
+
+var usage = `
+Usage:
+    songs --register [--no-add] <name>
+    songs
+    songs --added-at
+    songs --favourite
+    songs --frecent
+    songs --suggestions <name>
+Options:
+    -h --help         Show this screen.
+    -r --register     Register that you just heard a song. If the song does not
+                      exist yet, it will be added.
+    -n --no-add       Do not add a song to the database, when registering that
+                      you just heard it. If the song does not exist, nothing
+                      will happen.
+    -t --added-at     List songs by the date of their addition. Newest first.
+    -f --favourite    List songs you heard the most. Most heard first.
+    -c --frecent      List songs you recently heard a lot. Most frecent first.
+    -s --suggestions  List songs, that you often hear before or after hearing
+                      the given song. Best suggestions first.
+`
+
+type conf struct {
+	Name        string
+	Register    bool
+	NoAdd       bool
+	AddedAt     bool
+	Favourite   bool
+	Frecent     bool
+	Suggestions bool
+}
+
+func main() {
+	opts, err := docopt.ParseDoc(usage)
+	if err != nil {
+		// No need to print anything here, since ParseDoc() already does.
+		os.Exit(1)
+	}
+	var conf conf
+	err = opts.Bind(&conf)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, `Error when using arguments:`, err.Error())
+		os.Exit(2)
+	}
+
+	// TODO: Store database in $XDG_DATA_HOME .
+	db, err := songs.InitDB("test.sql")
+	defer db.Close()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, `Error when initializing database:`, err.Error())
+		os.Exit(3)
+	}
+	err = db.CreateSchemaIfNotExists()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, `Error when creating database schema:`, err.Error())
+		os.Exit(4)
+	}
+
+	switch {
+	case conf.Register && conf.NoAdd:
+		err = db.AddHearing(conf.Name)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, `Error when adding hearing:`, err.Error())
+			os.Exit(5)
+		}
+	case conf.Register:
+		err = db.AddHearingAndSongIfNeeded(conf.Name)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, `Error when adding song or hearing:`, err.Error())
+			os.Exit(6)
+		}
+	case conf.AddedAt:
+		songs, err := db.ListSongsInOrderOfAddition()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, `Error when listing songs:`, err.Error())
+			os.Exit(7)
+		}
+		for _, s := range songs {
+			fmt.Println(s)
+		}
+	case conf.Favourite:
+	case conf.Frecent:
+	case conf.Suggestions:
+	default:
+	}
+}
