03f040e78e2dece18b0ce319389d025fd146565b
codesoap <codesoap@mailbox.org>
2019-11-03 09:19:19 +0100

    Fix linting warnings in frecency.go

--- frecency.go
+++ frecency.go
@@ -21,23 +21,23 @@ func frecencyInputsToSongs(fis []frecencyInput) []string {
 	now := time.Now()
 	const lambda float64 = 0.00096270442 // (ln 2) / (30 days * 24h)
 
-	songIdToFrecency := make(map[string]float64)
+	songIDToFrecency := make(map[string]float64)
 	for _, fi := range fis {
-		var hearingAge float64 = now.Sub(fi.Date).Hours()
-		songIdToFrecency[fi.Name] += math.Exp(-lambda * hearingAge)
+		hearingAge := now.Sub(fi.Date).Hours()
+		songIDToFrecency[fi.Name] += math.Exp(-lambda * hearingAge)
 	}
 
-	// Make songIdToFrecencyKv a slice of songs sorted by frecency:
-	songIdToFrecencyKv := make([]kv, 0, len(songIdToFrecency))
-	for key, val := range songIdToFrecency {
-		songIdToFrecencyKv = append(songIdToFrecencyKv, kv{key, val})
+	// Make songIDToFrecencyKv a slice of songs sorted by frecency:
+	songIDToFrecencyKv := make([]kv, 0, len(songIDToFrecency))
+	for key, val := range songIDToFrecency {
+		songIDToFrecencyKv = append(songIDToFrecencyKv, kv{key, val})
 	}
-	sort.Slice(songIdToFrecencyKv, func(i, j int) bool {
-		return songIdToFrecencyKv[i].Val > songIdToFrecencyKv[j].Val
+	sort.Slice(songIDToFrecencyKv, func(i, j int) bool {
+		return songIDToFrecencyKv[i].Val > songIDToFrecencyKv[j].Val
 	})
 
-	songs := make([]string, 0, len(songIdToFrecencyKv))
-	for _, kv := range songIdToFrecencyKv {
+	songs := make([]string, 0, len(songIDToFrecencyKv))
+	for _, kv := range songIDToFrecencyKv {
 		songs = append(songs, kv.Key)
 	}
 	return songs
